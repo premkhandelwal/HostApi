08935b05cbefae930f423756f421453f3b2b1e48
prem.dkhandelwal@gmail.com <prem.dkhandelwal@gmail.com>
2021-07-03 16:01:42 +0530

    First Commit

---
+++ PremProjectApi1/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PremProjectApi1.Controllers
+{
+    [Route("api/Name")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public String NewItem()
+        {
+            return "My Name is Khan";
+        }
+        }
+}
